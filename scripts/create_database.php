554598c5b5b9a91cb21275561259c64ac0a7c3c7
desa-thomas <desa2thomas@gmail.com>
2025-02-27 13:51:45 -0500

    Template folders and files for backend

---
+++ scripts/create_database.php
@@ -0,0 +1,10 @@
+<?php
+
+$datafile_dir = __DIR__ . "/../proj data files";
+
+$file = fopen($datafile_dir . "/CourseTable.txt", "r") or die("Unable to open file");
+
+while(!feof($file)){
+    
+}
+?>
