4900674268872d9c0315d5d9e28658bb17ccb719
desa-thomas <desa2thomas@gmail.com>
2025-03-09 11:48:13 -0400

    Finished delete_course db_function, started `modify_grades()`

--- scripts/db_functions.php
+++ scripts/db_functions.php
@@ -9,29 +9,71 @@ I am assuming you cannot make changes to the NameTable Table
 i.e., You can't modify Students names or IDs
 */
 
+$_ENV = parse_ini_file(__DIR__ . "/../.env");
 
 /**
  * Delete a student's course record from database
  * 
  * @param string $id         9 digit Student ID
  * @param string $coursecode Course code of course to delete, format (cciii)
+ * 
+ * @return array(bool, string) $deleted - (true if deleted, message)
  */
 function delete_course(string $id, string $coursecode){
+    try{
+        $conn = new mysqli($_ENV["SERVER"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DB_NAME"]); 
+    }catch(Exception $e){
+        return [false, $e->getMessage()];
+    }
 
+    $stmt = $conn->prepare("DELETE FROM CourseTable WHERE (Studentid = ? and courseCode = ?)"); 
+    $stmt->bind_param("ss", $id, $coursecode);
+    $stmt->execute();
+
+    /**Rows affected by DELETE statement */
+    $affected_rows = $conn->affected_rows;
+    $conn->close(); 
+
+    if($affected_rows == 1){
+        return[true, "Course Deleted"];
+    }
+    else{
+        return[false, "Course or Student not found"];
+    }
 }
 
 /**
- * Modify a students grade for a particular test in a course
+ * Modify a student's grade for a particular test in a course
  * 
  * @param string $id         9 digit Student ID
  * @param string $coursecode Course code for the class for which grade to udpate (cciii)
- * @param int    $test_no    Test no of test which grade we want to update.
+ * @param array(float)  $new_vals  Associative array of updated values e.g., 
+ *                           ["test1" => updated_value,
+ *                            "test2" => updated value,
+ *                            "test3" => updated value,
+ *                            "finalExam" => updated value]    
+ *                            Null values for entries that won't be udpated
+ *      
  *                           1, 2, 3, for test1, test2, and test3 repectively & 4 for the final exam
- * @param float  $new_grade  Value of the new grade
+ * 
+ * @return array(bool, string) $modified  [column_modified, message]
  */
-function modify_grade(string $id, string $coursecode, int $test_no, float $new_grade){
+function modify_grades(string $id, string $coursecode, array $new_vals){
+    $key_template = ["test1", "test2", "test3", "finalExam"]
 
-    return
+    if(array_keys($new_vals) != $key_template){
+        return [false, "Associative array keys should have format:\n". '["test1", "test2", "test3", "finalExam"]']
+    }
+    try{
+        $conn = new mysqli($_ENV["SERVER"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DB_NAME"]); 
+    }catch(Exception $e){
+        return [false, $e->getMessage()];
+    }
+
+    foreach($new_vals as $key=> $value){
+            
+    }
+    return 
 }
-?>
 
+?>
