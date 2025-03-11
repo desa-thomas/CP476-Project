ef8259c37cf606709eb373b16798c926144fe0e3
desa-thomas <desa2thomas@gmail.com>
2025-03-11 19:47:03 -0400

    started php script searchs db from user input and displays results

---
+++ frontend/searchresults.js
@@ -0,0 +1,10 @@
+/**
+ * Date: March 11 2025
+ * Authors: Thomas De Sa,
+ * 
+ * Client Script for Search results page
+ */
+
+onload = ()=>{
+    console.log(search_results)
+}
--- scripts/search.php
+++ scripts/search.php
@@ -1,9 +1,26 @@
-<?php
-/**
- * Date: March 11 2025
- * Author: Thomas De Sa
- */
+<!-- 
+Date: March 11 2025
+Authors: Thomas De Sa,
 
-echo $_POST["searchbar"]; 
+Search for Student by ID in DB, then return HTML page displaying those results
+-->
+<!DOCTYPE html>
+
+<?php include("db_functions.php");
+
+//Get search value from URL parameters
+$id = $_GET["id"];
+$ids = search_id($id);
+
+//pass search results to javascript for dynamic loading of content
+$json = json_encode($ids);
+echo "<script>let search_results = $json</script>";
 ?>
 
+
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="../frontend/styles.css">
+    <script src="../frontend/searchresults.js"></script>
+</head>
