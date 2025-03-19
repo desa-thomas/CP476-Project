b588e99afdb44f96f571bc055e61614495a21312
desa-thomas <desa2thomas@gmail.com>
2025-03-19 12:48:56 -0400

    created student.php

---
+++ pages/student.php
@@ -0,0 +1,15 @@
+<!-- 
+Date: March 19 2025
+Authors: Thomas De Sa,
+
+Page to display and modify student information (grades).
+
+Page is accessed by clicking on the student card of a search result
+from search.php -->
+<!DOCTYPE html>
+
+<?php
+include("../scripts/db_functions.php")
+
+$id = $_GET["id"]
+?>
